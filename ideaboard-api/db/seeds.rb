f700a4276e595948a29120e44be821004f1789db
Greg Cosgrove <gregcosgrove@gmail.com>
2017-09-01 15:53:04 -0400

    Demo Rails-React App

---
+++ ideaboard-api/db/seeds.rb
@@ -0,0 +1,19 @@
+ideas = Idea.create(
+  [
+    {
+      title: "A new cake recipe",
+      body: "Made of chocolate"
+    },
+    {
+      title: "A twitter client idea",
+      body: "Only for replying to mentions and DMs"
+    },
+    {
+      title: "A novel set in Italy",
+      body: "A mafia crime drama starring Bob"
+    },
+    {
+      title: "Card game design",
+      body: "Like uno for alcoholics"
+    }
+  ])
